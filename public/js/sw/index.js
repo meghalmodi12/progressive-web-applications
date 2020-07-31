67e25f5c39120c9c78bb3da3e7332009309d2320
meghalmodi12 <meghalmodi12@gmail.com>
2020-07-31 17:44:03 -0400

    Caching urls using service worker

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,19 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open('wittr-static-v1')
+            .then(cache => {
+                return cache.addAll([
+                    '/',
+                    'js/main.js',
+                    'css/main.css',
+                    'imgs/icon.png',
+                    'https://fonts.gstatic.com/s/roboto/v15/2UX7WLTfW3W8TclTUvlFyQ.woff',
+                    'https://fonts.gstatic.com/s/roboto/v15/d-6IYplOFocCacKzxwXSOD8E0i7KZn-EPnyo3HZu7kw.woff'
+                ]);
+            })
+    );
+});
+
 self.addEventListener('fetch', (event) => {
     console.log(event.request);
     event.respondWith(
