b9ddfce8d392a529d9f2ac048d1b91697cba8233
meghalmodi12 <meghalmodi12@gmail.com>
2020-07-31 14:49:43 -0400

    Hijacking requests for .jpg files and sending .gif file in return

--- public/js/sw/index.js
+++ public/js/sw/index.js
@@ -1,3 +1,9 @@
 self.addEventListener('fetch', (event) => {
     console.log(event.request);
+    if (event.request.url.endsWith('.jpg')) {
+        event.respondWith(
+            fetch('/imgs/dr-evil.gif')
+        );
+    }
+    
 });
